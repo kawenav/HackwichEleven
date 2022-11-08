4df1f2586c8206f9426710219a6d4ec1278efedc
kawenav <111808380+kawenav@users.noreply.github.com>
2022-11-07 15:05:58 -1000

    I completed Hackwich 11 and Problem Set #1

--- HackwichEleven/ViewController.swift
+++ HackwichEleven/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
-
+import MapKit
+import CoreLocation
+ 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.055725)
+    
+    let regionRadius = 10000
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        centerMapOnLocation(location: initialLocation)
         // Do any additional setup after loading the view.
+        
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2DMake(21.346470, -158.080098))
+        mapView.addAnnotation(restaurantOne)
+        
+        let restaurantTwo = Restaurant(title: "Kalapawai Market", type: "Cafe", coordinate: CLLocationCoordinate2DMake(21.39736, -157.72982))
+        mapView.addAnnotation(restaurantTwo)
+               
+        
     }
 
 
+    func centerMapOnLocation(location:CLLocation){
+        
+        let coordinationRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: CLLocationDistance(regionRadius), longitudinalMeters: CLLocationDistance(regionRadius))
+        mapView.setRegion(coordinationRegion, animated: true)
+        
+    }
+    
+    
 }
 
